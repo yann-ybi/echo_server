b7b5e56b4003f7bfc0ad106f14178f7bb209634d
Yann Youbi <youbi.yann@gmail.com>
2022-12-23 18:36:28 -0500

    Unresponsive ping every 15 seconds
    close connection when pong not received from client

--- src/main.rs
+++ src/main.rs
@@ -2,28 +2,48 @@ extern crate ws;
 use std::{thread, time};
 use ws::util::{Timeout, Token};
 use ws::{
-    CloseCode, Error, ErrorKind, Handler, Handshake, Result, Sender, WebSocket
+    CloseCode, Error, ErrorKind, Frame, Handler, Handshake, OpCode, Result, Sender, WebSocket
 };
 
 const PING: Token = Token(0);
+const CLIENT_UNRESPONSIVE: Token = Token(1);
+
 
 // The factory worker from the pool sending message to the server
 struct Server {
     out: Sender,
     ping_timeout: Option<Timeout>,
+    client_unresponsive_timeout: Option<Timeout>
 }
 
 impl Handler for Server {
     fn on_open(&mut self, _: Handshake) -> Result<()> {
+        println!("Opened a connection");
+        self.out.timeout(15_000, CLIENT_UNRESPONSIVE)?;
         self.out.timeout(5_000, PING)
     }
     
     fn on_timeout(&mut self, event: Token) -> Result<()> {
+        println!("event: {:?}", event);
         match event {
             PING => {
                 println!("Pinging the client");
                 self.out.ping("".into())?;
-                self.out.timeout(5_000, PING)
+
+                match self.client_unresponsive_timeout {
+                    Some(_) => self.out.timeout(5_000, PING),
+                    None => Ok(())
+                }
+            }
+            CLIENT_UNRESPONSIVE => {
+                println!("Client is unresponsive, closing connection");
+                self.client_unresponsive_timeout.take();
+                if let Some(timeout) = self.ping_timeout.take() {
+                    println!("timeout: {:?}", timeout);
+                    self.out.cancel(timeout)?;
+                    println!("canceled");
+                }
+                self.out.close(CloseCode::Away)
             }
             _ => Err(Error::new(
                ErrorKind::Internal,
@@ -33,18 +53,36 @@ impl Handler for Server {
     }
 
     fn on_new_timeout(&mut self, event: Token, timeout: Timeout) -> Result<()> {
+        println!("new timeout: {:?}", timeout);
         match event {
             PING => {
                 if let Some(timeout) = self.ping_timeout.take() {
                     self.out.cancel(timeout)?
                 }
-                self.ping_timeout = Some(timeout);
+                match self.client_unresponsive_timeout {
+                    Some(_) => self.ping_timeout = Some(timeout),
+                    None => self.ping_timeout = None
+                }
+            }
+            CLIENT_UNRESPONSIVE => {
+                if let Some(timeout) = self.client_unresponsive_timeout.take() {
+                    self.out.cancel(timeout)?
+                }
+                self.client_unresponsive_timeout = Some(timeout)
             }
             _ => {
                 eprintln!("Unknown event: {:?}", event);
             }
         }   
         Ok(())
+    }
+
+    fn on_frame(&mut self, frame: Frame) -> Result<Option<Frame>> {
+        if frame.opcode() == OpCode::Pong {
+            println!("Received a pong");
+            self.out.timeout(15_000, CLIENT_UNRESPONSIVE)?;
+        }
+        Ok(Some(frame))
     }
 
     fn on_close(&mut self, code: CloseCode, reason: &str){
@@ -56,12 +94,12 @@ impl Handler for Server {
 }
 
 fn main() {
-    let server = WebSocket::new(|out| Server { out: out, ping_timeout: None }).unwrap();
+    let server = WebSocket::new(|out| Server { out: out, ping_timeout: None, client_unresponsive_timeout: None }).unwrap();
 
     let broadcaster = server.broadcaster();
     
     let periodic = thread::spawn(move || loop {
-        broadcaster.send("It's Yann's web app!").unwrap();
+        broadcaster.send("Yann's web app!").unwrap();
         thread::sleep(time::Duration::from_secs(1));
     });
 
